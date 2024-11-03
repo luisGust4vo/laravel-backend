72a1b3ca36f00f87b2abd9f70a260c5d517e7331
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2024-11-03 00:40:46 -0300

    Feat:Criacao das migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return "ola mundo";
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
+Route::get('/email', function () {
+    Mail::raw('TESTE DE EMAIL LUIS', function (Message $message) {
+        $message->to('luis@localhost') // Use um e-mail que você quer testar
+                ->subject('Bem vindo')
+                ->from('rh@testinho.com'); // Use um endereço de e-mail válido
+    });
+    echo "Email enviado";
 });
+
+
